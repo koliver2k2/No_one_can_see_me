a41dd5405bd2d6cf99e16ac286901492812b07b3
koliver2k2 <klagoliver2@gmail.com>
2026-06-09 16:34:44 +0200

    Small changes, added background

--- Scripts/character_movement.gd
+++ Scripts/character_movement.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-const RUN_SPEED := 330
+const RUN_SPEED := 280
 const WALK_SPEED := 180
 const CLIMB_SPEED := 150
 const JUMP_VELOCITY := -350
-const RUN_JUMP_VELOCITY := -500
+const RUN_JUMP_VELOCITY_X = 520
 const WALL_JUMP_X := 400
 const WALL_JUMP_Y := -450
 const GRAVITY := 1000
@@ -15,7 +15,7 @@ var is_running := false
 var jumps_left := MAX_JUMPS
 var is_double_jumping := false  # NEW
 
-@onready var animated_sprite = $AnimatedSprite2D
+@onready var animated_sprite = $Animated_Texture
 
 func handle_animations(direction):
 	if is_climbing:
@@ -83,10 +83,9 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("ui_accept") and jumps_left > 0 and not Input.is_action_pressed("crouch"):
 			is_double_jumping = jumps_left < MAX_JUMPS
 			jumps_left -= 1
-			if not is_running:
-				velocity.y = JUMP_VELOCITY
-			else:
-				velocity.y = RUN_JUMP_VELOCITY
+			velocity.y = JUMP_VELOCITY
+			if is_running:
+				velocity.x = direction * RUN_JUMP_VELOCITY_X
 
 		if is_running and is_on_floor():
 			velocity.x = direction * RUN_SPEED
